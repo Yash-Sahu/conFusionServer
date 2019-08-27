9f3e3928414d55a0fc040055d23ee72b47d01ab7
Amit Sahu <amitsahu99900@gmail.com>
2019-08-28 01:13:31 +0530

    favourite router

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favouriteRouter');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -80,6 +81,7 @@ app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favourites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
