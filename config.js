277a4558b81c6cd46a6c618c4233b82c5b029f92
Amit Sahu <amitsahu99900@gmail.com>
2019-08-29 14:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey':'12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId : '419129492294584',
-        clientSecret : 'd88c29fe064e874fb90d1f74cab3a0c5'
+        clientId : '',
+        clientSecret : ''
     }
 }
